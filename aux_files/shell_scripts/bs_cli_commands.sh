f597f94aecca7a72636602276d2de81686385f88
vitu1 <vtu292@gmail.com>
2022-08-30 14:23:23 -0400

    changed coverage calculation by using bed file. Automated more steps like moving fastq files to upload

--- aux_files/shell_scripts/bs_cli_commands.sh
+++ aux_files/shell_scripts/bs_cli_commands.sh
@@ -3,11 +3,17 @@ PROJECT_ID=$(HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe projects create -n $1
 
 echo "BaseSpace Project was created with ID $PROJECT_ID"
 
+sleep 30
+
 #upload the data to the project ID
 HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe upload dataset -p $PROJECT_ID --recursive $2
 
+sleep 30
+
 #get the latest DRAGEN app version
 APP_VERSION=$(HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe application list -f csv | grep "$3" | cut -d, -f3)
+
+sleep 30
 
 #CSV format doesn't return what we want nicely to using json format; the first Id should be the AppSession number
 APP_SESSION_ID=$(HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe launch application -n "$3" --app-version $APP_VERSION \
@@ -18,24 +24,25 @@ APP_SESSION_ID=$(HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe launch applicatio
   -o basespace-labs-disclaimer:Accepted \
   -f json | grep Id | cut -d'"' -f4 | head -n1)
 
-HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe await $APP_SESSION_ID
+sleep 30
 
-HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe download project -i $PROJECT_ID \
-  --extension=detect.report.csv \
-  --extension=lineage_report.csv \
-  --extension=nextclade.tsv \
-  --extension=json \
-  --exclude "kmer_config*" \
-  -o "$5" \
-  --no-metadata \
-  --overwrite
+#wait until the app finishes running
+HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe await appsession $APP_SESSION_ID
+
+sleep 30
 
 HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe download project -i $PROJECT_ID \
   --exclude "*" \
-  --include "ScrubbedFASTQsNode*.zip" \
+  --include "*.zip" \
+  --include "Undetermined_report_metrics.json" \
+  --include "*detect.report.csv" \
+  --include "*lineage_report.csv" \
+  --include "*nextclade.tsv" \
   -o "$5" \
   --no-metadata \
   --overwrite
+
+sleep 30
 
 APP_DATE=$(HTTPS_PROXY=proxy.phila.gov:8080 $HOME/bs.exe appsession get --id $APP_SESSION_ID -f json | grep DateCompleted | cut -d'"' -f4)
 
