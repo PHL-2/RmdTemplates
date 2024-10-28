fdd2b2bbc2cc41ae9e00558dad68bf4fb5784c4d
chrgu <33235314+chrgu@users.noreply.github.com>
2024-10-28 16:19:25 -0400

    test

--- inst/rmarkdown/templates/covid_cecret_wastewater/skeleton/code/2_generate_barcodes_IDT.R
+++ inst/rmarkdown/templates/covid_cecret_wastewater/skeleton/code/2_generate_barcodes_IDT.R
@@ -22,7 +22,7 @@ have_AWS_EC2_SSH_access <- TRUE
 
 remove_sample_from_bcl_samplesheet <- c("") #add in sample names to remove from demultiplexing
 
-sample_w_empty_reads <- c("") #add in sample ids that have empty fastq files
+sample_w_empty_reads <- c(" ") #add in sample ids that have empty fastq files
 
 # temporary directory to hold the sequencing run download
 ec2_tmp_fp <- "~/tmp_bs_dl"
