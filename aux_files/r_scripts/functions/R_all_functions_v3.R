e9107ed8eb3d1ba6d6ef700f5b05bc678891a3cb
Vincent Tu <vtu292@gmail.com>
2024-12-27 14:18:59 -0500

    decrease screen height

--- aux_files/r_scripts/functions/R_all_functions_v3.R
+++ aux_files/r_scripts/functions/R_all_functions_v3.R
@@ -194,7 +194,7 @@ run_in_terminal <- function(command2run = "", command2print = "") {
 submit_screen_job <- function(message2display = "Running function to submit screen job",
                               ec2_login = "", screen_session_name = "", screen_log_fp = "~/.tmp_screen",
                               command2run = "",
-                              window_height = 100,
+                              window_height = 40,
                               window_length = 120) {
 
   run_in_terminal(paste("echo", paste0("'", message2display, " through a screen command on EC2 instance [", ec2_login, "]';"),
