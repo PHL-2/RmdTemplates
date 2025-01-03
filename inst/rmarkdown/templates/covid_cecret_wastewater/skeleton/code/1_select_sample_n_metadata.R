2c669ff5843440ae1507464adf08249d87acb28a
Vincent Tu <vtu292@gmail.com>
2025-01-03 15:43:29 -0500

    increase ddPCR runs to load to get metadata

--- inst/rmarkdown/templates/covid_cecret_wastewater/skeleton/code/1_select_sample_n_metadata.R
+++ inst/rmarkdown/templates/covid_cecret_wastewater/skeleton/code/1_select_sample_n_metadata.R
@@ -53,7 +53,7 @@ tryCatch(
 failed_regex <- "test|exclude"
 
 ddPCR_files <- list.files(ddPCR_run_fp, pattern = ".*_ww_sequencing_metadata.csv", full.names = TRUE, recursive = TRUE)
-ddPCR_files <- tail(ddPCR_files[!grepl(failed_regex, ddPCR_files)], 10)
+ddPCR_files <- tail(ddPCR_files[!grepl(failed_regex, ddPCR_files)], 50)
 
 ddPCR_data <- ddPCR_files %>%
   data_frame(FileName = .) %>%
