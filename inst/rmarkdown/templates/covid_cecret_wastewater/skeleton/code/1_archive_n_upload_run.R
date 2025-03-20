1ed678fe81b333a0a3f8f1393c7e96840867abbf
Vincent Tu <vtu292@gmail.com>
2025-03-20 09:59:44 -0400

    merge from main (#284)
    * fixed Rscript name
    
    * include pipe library
    
    * changed library to dplyr
    
    * use correct option for matching file with pathname
    
    * minor edit
    
    * Will look for a RunCompletionStatus.xml file first to see if a run has completed
    
    * small update to message
    
    * update finding run on BaseSpace
    
    * include tidyverse library
    
    * fixed empty environmental df to merge
    
    * fixed errors relating to checking for missing ddPCR data and uploading the sample sheet to S3 bucket
    
    * applied sample sheet changes to nasal swab template
    
    * return to console after running screen through terminal
    
    * removed all aws cli commands running locally; only allow function to run through ec2 instance

--- inst/rmarkdown/templates/covid_cecret_wastewater/skeleton/code/1_archive_n_upload_run.R
+++ inst/rmarkdown/templates/covid_cecret_wastewater/skeleton/code/1_archive_n_upload_run.R
@@ -1,4 +1,5 @@
 library(here)
+library(dplyr)
 
 #This Rscript uploads the sequencing run folder and related files to S3 and makes a record of the run on BaseSpace
 
@@ -16,9 +17,9 @@ if(sequencing_date == "" | is.na(as.Date(sequencing_date, "%Y-%m-%d")) | nchar(s
   stop(simpleError(paste0("Please use the 'YYYY-MM-DD' format for this RStudio project date. This date should correspond to the sequencing run date")))
 }
 
-###################################################
+################
 # Load functions
-###################################################
+################
 
 #this file needs to sit in a [aux_files/r_scripts/functions] directory path above this project directory
 tryCatch(
@@ -30,9 +31,9 @@ tryCatch(
   }
 )
 
-###################################################
+#############
 # Load config
-###################################################
+#############
 
 #this file needs to sit in a [aux_files/r_scripts/config] directory path above this project directory
 tryCatch(
@@ -94,7 +95,7 @@ if(is.null(check_run_on_s3)) {
   stop(simpleError(paste(selected_sequencer_type, "run detected in", s3_run_bucket_fp,
                          "\nTo create a new tarball, manually delete the old file in", s3_run_bucket_fp)))
 }
-message("Run on S3 not found. Continuing to create tarball")
+message("\nRun on S3 not found. Continuing to create tarball")
 
 system2("ssh", c(ec2_hostname,
                  shQuote(
