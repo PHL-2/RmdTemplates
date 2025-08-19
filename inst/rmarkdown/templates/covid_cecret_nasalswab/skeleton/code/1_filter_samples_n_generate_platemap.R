4a0e054aeb4cc8aa60b52a7e6789e37e565315c7
vitu1 <vtu292@gmail.com>
2025-08-19 14:49:17 -0400

    remove BLANK sample

--- inst/rmarkdown/templates/covid_cecret_nasalswab/skeleton/code/1_filter_samples_n_generate_platemap.R
+++ inst/rmarkdown/templates/covid_cecret_nasalswab/skeleton/code/1_filter_samples_n_generate_platemap.R
@@ -445,7 +445,7 @@ plate_view <- combined_list_first_half %>%
   mutate(number = cumsum(duplicated(sample_name)) + 1) %>%
   mutate(sample_name = ifelse(sample_name == "NC-pre-extract", paste0(sample_name, number), sample_name)) %>%
   select(-number) %>%
-  rbind(data.frame(sample_name = c("BLANK", "PC", "NC-pre-cDNA", "NC-pre-ARTIC", "NC-pre-library"))) %>%
+  rbind(data.frame(sample_name = c("PC", "NC-pre-cDNA", "NC-pre-ARTIC", "NC-pre-library"))) %>%
   mutate(sample_order = row_number()) %>%
   merge(empty_plate, by = "sample_order", all = TRUE) %>%
   mutate(sample_name = case_when(sample_order == 96 ~ "NC-corner",
